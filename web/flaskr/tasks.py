c8c700edf9ecec067636e8bef8578bc7ef460cba
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2023-10-24 19:36:35 -0700

    Update tasks.py

--- web/flaskr/tasks.py
+++ web/flaskr/tasks.py
@@ -16,35 +16,3 @@ def update_feeds():
         time_end = datetime.utcnow().replace(microsecond=0)
         print(f'All feeds updated {time_end.isoformat()}\n')
 
-
-'''
-# create tasks for existing companies
-def add_gtfs_tasks_existing():
-    feeds = db.session.query(Feed).all()
-    for feed in feeds:
-        add_gtfs_tasks(feed.id)
-
-
-# add new task
-# if we allow each job to be executed by different threads(), then put lock on add_vehicle function
-def add_gtfs_tasks(feed_id: int):
-    scheduler.add_job(
-        func=update_vehicle_position,
-        trigger="cron",
-        second=0,
-        args=[feed_id],
-        id=f"{feed_id}_update_vehicle_positions",
-        name=f"{feed_id}: update vehicle positions",
-        replace_existing=True
-    )
-    scheduler.add_job(
-        func=update_trip_updates,
-        trigger="cron",
-        second=0,
-        args=[feed_id],
-        id=f"{feed_id}_update_trip_updates",
-        name=f"{feed_id}: update trip records and stop distance",
-        replace_existing=True
-    )
-    print(f"Tasks Added for {feed_id}")
-'''
